58370a55aa837fa04595a1fd93d2c4d9fffad4e9
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2022-07-21 14:43:57 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: "mongodb+srv://ttpuser:Ttpuser1234@concorddb.fkeeqds.mongodb.net/?retryWrites=true&w=majority"
+  mongoUri: "mongodb+srv://myuser:<<mypassword>>@demoDB.somesubdomain.mongodb.net/?retryWrites=true&w=majority"
 };
 
 export default config;
